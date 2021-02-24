0ce97caafd25bb8eb16d32b32cb93ee11d7be1a0
Dmitry Bulatov <59661257+Apozon@users.noreply.github.com>
2021-02-24 20:14:50 +0700

    Update random_array.kt

--- random_array.kt
+++ random_array.kt
@@ -8,19 +8,19 @@ import kotlin.random.Random
 Массивы с четным порядковым номером отсортировать по возрастанию, с нечетным порядковым номером - по убыванию.
 На выходе функция должна вернуть массив с отсортированными массивами.
 */
-    fun random_array (n:Int) : ArrayList<Array<Int>> {
-        if (n < 1) return ArrayList()
-        var array_size = MutableList<Int>(1024, { i -> i+1})
-        Collections.shuffle(array_size)
-        val shuffle_array = array_size.toTypedArray()
-        var main_array = ArrayList<Array<Int>>(n)
-        for (id in 0..n) {
-            main_array.add(Array(shuffle_array[id], { Random.nextInt() }))
-        }
-                for (main_id in 0..n step 2) main_array[main_id].sortedArray()
-                for (main_id in 1..n step 2) main_array[main_id].sortedArrayDescending()
-                return main_array
+ fun random_array (n:Int) : ArrayList<Array<Int>> {
+    if (n < 1) return ArrayList()
+    var array_size = MutableList<Int>(1024, { i -> i+1})
+    Collections.shuffle(array_size)
+    val shuffle_array = array_size.toTypedArray()
+    var main_array = ArrayList<Array<Int>>(n-1)
+    for (id in 0..n-1) {
+        main_array.add(Array(shuffle_array[id], { Random.nextInt() }))
     }
+    for (main_id in 0..n-1 step 2) main_array[main_id].sortedArray()
+    for (main_id in 1..n-1 step 2) main_array[main_id].sortedArrayDescending()
+    return main_array
+}
 
 
 
